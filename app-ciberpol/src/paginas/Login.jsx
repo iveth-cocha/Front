b32420b3ad3eddbf5979138a60ef599d56f663ca
ivet-cocha <maria.iveth16@outlook.es>
2024-04-07 14:53:54 -0500

    inicializacion servidor

---
+++ app-ciberpol/src/paginas/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <div  className="h-screen bg-cover bg-center" style={{backgroundImage: `url('/fondo.jpg')`}} >
+        <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-3xl font-bold underline text-white">Acceso</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Login
