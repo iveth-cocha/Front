48ed292368e6b21a70824067da56718d007ea9ce
ivet-cocha <maria.iveth16@outlook.es>
2024-04-12 17:40:31 -0500

    cambios tabla delegaciones

--- app-ciberpol/src/componets/TablaDelegaciones.jsx
+++ app-ciberpol/src/componets/TablaDelegaciones.jsx
@@ -7,18 +7,22 @@ const TablaDelegaciones = () => {
     <table className='w-full mt-5 table-auto shadow-lg  bg-white'>
             <thead className='bg-sky-950 text-white'>
                 <tr>
-                    <th className='p-2'>N°</th>
-                    <th className='p-2'>N° de instrucción fiscal</th>
-                    <th className='p-2'>N° de investigación previa</th>
+                    <th className='p-2'>N° de orden</th>
+                    <th className='p-2'>Año Ingreso</th>                    
+                    <th className='p-2'>N° Instrucción Fiscal</th>
+                    <th className='p-2'>N° Investigación Previa</th>
                     <th className='p-2'>Grado</th>
                     <th className='p-2'>Agente</th>
+                    <th className='p-2'>Acciones</th>
                 </tr>
             </thead>
             <tbody>
                 <tr className="border-b hover:bg-gray-300 text-center">
-                    <td>nombre</td>
-                    <td>nombre</td>
-                    <td>nombre</td>
+                    <td>1</td>
+                    <td>2024</td>
+                    <td>002</td>
+                    <td>00023</td>
+                    <td>Gral</td>
                     <td>nombre</td>
                     <td className='py-2 text-center'>
                         <VscOpenPreview className="h-7 w-7 text-blue-800 cursor-pointer inline-block mr-2"/>
