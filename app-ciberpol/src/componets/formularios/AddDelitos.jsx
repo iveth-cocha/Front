a4601afdda470ef73754e76a9f46ef02873ae473
ivet-cocha <maria.iveth16@outlook.es>
2024-04-15 21:36:43 -0500

    responsive para Agregardelito

--- app-ciberpol/src/componets/formularios/AddDelitos.jsx
+++ app-ciberpol/src/componets/formularios/AddDelitos.jsx
@@ -4,18 +4,18 @@ const AddDelitos = () => {
   return (
     <div >
         <form >
-            <div className='flex px-10'>
-                  <div className=' mr-3'>
-                      <label className='mr-2'>Delito</label>
+            <div className='flex flex-row flex-wrap  gap-8'>
+                  <div >
+                      <label >Delito</label>
                       <input type="String" className="w-15 rounded-md border border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500" />
                   </div>
-                  <div className=' mx-3'>
-                      <label className='mr-2'>Sección</label>
+                  <div >
+                      <label >Sección</label>
                       <input type="String" className=" rounded-md border border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500" />
                   </div>
 
-                  <div className='mx-5'>
-                      <button className="py-2 w-20 block text-center bg-blue-950 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-sky-950 hover:text-white">Agregar</button>
+                  <div >
+                      <button className="py-2 w-20 text-center bg-blue-950 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-sky-950 hover:text-white">Agregar</button>
                   </div>
             </div>
             
