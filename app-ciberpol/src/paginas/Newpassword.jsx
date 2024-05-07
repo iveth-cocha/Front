087c63009d172fbe302f77099cb915046d26c342
ivet-cocha <maria.iveth16@outlook.es>
2024-05-07 04:12:44 -0500

    actualizacion contraseña

--- app-ciberpol/src/paginas/Newpassword.jsx
+++ app-ciberpol/src/paginas/Newpassword.jsx
@@ -1,40 +1,81 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom'; 
+import axios from 'axios';
+import Mensaje from '../componets/Alertas/Mensaje';
 
 const Newpassword = () => {
+    const { token } = useParams(); 
+  
+    const [mensaje, setMensaje] = useState({})
+    const [form, setForm] = useState({
+        newPassword: '',
+        confirmPassword: ''
+      });
+    
+      const handleChange = (e) => {
+        setForm({
+          ...form,
+          [e.target.name]: e.target.value
+        });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const url = `${import.meta.env.VITE_BACKEND_URL}/actualizar-contrasena/${token}`;
+          const response = await axios.put(url, form); // Envía los datos del formulario en la solicitud
+          setMensaje({ respuesta: response.data.msg, tipo: true });
+        } catch (error) {
+          setMensaje({ respuesta: error.response.data.msg, tipo: false });
+        }
+      };
+
   return (
-    <div className="h-screen bg-cover bg-center font-serif" style={{ backgroundImage: `url('/fondo_base.jpg')` }} >
-            <div className="flex flex-col justify-center items-center h-full">
-                <div className='flex flex-row flex-wrap  gap-8 mb-3'>
-                    <img src="/logo_policia.png" className=' h-32' />
-                    <img src="/logo_ciberpol.png" className='h-32' />
-                </div>
-                <h1 className="text-3xl font-bold underline text-red mb-3">SISTEMA DE DELEGACIONES </h1>
-
-                <h2 className="text-3xl font-bold text-red mb-4">Crear contraseña </h2>
-                <p className='uppercase'>agregar el token en la ruta para la funcionalida en el archivo app</p>
-
-                <form>
-                    <div className=' mb-3'>
-                        <label className='mr-4'>Nueva Contraseña</label>
-                        <input type="password" placeholder="**************" className="block w-full rounded-md border border-gray-300 focus:border-sky-900 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500" />
-                    </div>
-                    <div className=' mb-3'>
-                        <label className='mr-4'>Confirmar Nueva Contraseña</label>
-                        <input type="password" placeholder="**************" className="block w-full rounded-md border border-gray-300 focus:border-sky-900 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500" />
-                    </div>
-
-                    <div className="mx-8">
-                        <Link to="/" className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Cambiar Contraseña</Link>
-                    </div>
-                </form>
-
-                
-
-
-            </div>
+    <div className="h-screen bg-cover bg-center font-serif" style={{ backgroundImage: `url('/fondo_base.jpg')` }}>
+      <div className="flex flex-col justify-center items-center h-full">
+        <div className="flex flex-row flex-wrap gap-8 mb-3">
+          <img src="/logo_policia.png" className="h-32" alt="Logo Policía" />
+          <img src="/logo_ciberpol.png" className="h-32" alt="Logo Ciberpol" />
         </div>
-  )
-}
+        <h1 className="text-3xl font-bold underline text-red mb-3">SISTEMA DE DELEGACIONES</h1>
+        <h2 className="text-3xl font-bold text-red mb-4">Crear contraseña</h2>
+        {Object.keys(mensaje).length > 0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
 
-export default Newpassword
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="mr-4">Nueva Contraseña</label>
+            <input
+              type="password"
+              name="newPassword"
+              value={form.newPassword}
+              onChange={handleChange}
+              placeholder="**************"
+              className="block w-full rounded-md border border-gray-300 focus:border-sky-900 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="mr-4">Confirmar Nueva Contraseña</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={form.confirmPassword}
+              onChange={handleChange}
+              placeholder="**************"
+              className="block w-full rounded-md border border-gray-300 focus:border-sky-900 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500"
+            />
+          </div>
+
+          <div className="mx-8">
+            <button type="submit" className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Cambiar Contraseña</button>
+          </div>
+        </form>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Newpassword;
