64c1d5b84b8be2b7ddcad54fd94033db6a6ab703
ivet-cocha <maria.iveth16@outlook.es>
2024-05-22 22:56:00 -0500

    tabla con filtrado en delegaciones

--- app-ciberpol/src/componets/TablaDelegaciones.jsx
+++ app-ciberpol/src/componets/TablaDelegaciones.jsx
@@ -1,39 +1,25 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { MdDeleteForever, MdEditDocument } from 'react-icons/md';
-import { VscOpenPreview } from 'react-icons/vsc';
+import React, { useState, useEffect } from 'react';
+import { FilterMatchMode, FilterService } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
 import { useNavigate } from 'react-router-dom';
-import Mensaje from './Alertas/Mensaje';
-import ReactPaginate from 'react-paginate';
-
-const Paginator = ({ pageCount, handlePageClick, currentPage }) => {
-  return (
-    <ReactPaginate className="flex flex-row flex-wrap justify-center  bg-sky-950 text-white italic py-2 rounded-t-lg border-b-2 border-gray-500"
-      previousLabel={'Anterior <'}
-        nextLabel={'> Siguiente'}
-        breakLabel={`... Página ${currentPage + 1} de ${pageCount} ...`}
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        containerClassName={'pagination'}
-        activeClassName={'active'}
-        previousClassName={'prev'}
-        nextClassName={'next'}
-    />
-  );
-};
+import axios from 'axios';
+import { VscOpenPreview } from 'react-icons/vsc';
+import { MdEditDocument } from 'react-icons/md';
 
 const TablaDelegaciones = () => {
+
   const navigate = useNavigate();
 
-  const [delegaciones, setDelegaciones] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [perPage] = useState(10);
-  //const [perPage] = useState(8);
-  const [pageCount, setPageCount] = useState(0);
-
-  const handlePageClick = ({ selected }) => {
-    setCurrentPage(selected);
-  };
+  const [delegaciones, setDelegaciones] = useState(null);
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    numero_instruccion_fiscal: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    numero_investigacion_previa: { value: null, matchMode: FilterMatchMode.CONTAINS }
+  });
+  const [loading, setLoading] = useState(true);
+  const [globalFilterValue, setGlobalFilterValue] = useState('');
 
   const listarDelegaciones = async () => {
     try {
@@ -47,7 +33,7 @@ const TablaDelegaciones = () => {
       };
       const respuesta = await axios.get(url, options);
       setDelegaciones(respuesta.data);
-      setPageCount(Math.ceil(respuesta.data.length / perPage));
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -55,68 +41,96 @@ const TablaDelegaciones = () => {
 
   useEffect(() => {
     listarDelegaciones();
-  }, [currentPage, perPage]);
+  }, []);
 
+  const onGlobalFilterChange = (e) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
+
+    _filters['global'].value = value;
+
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
+
+  const headerNumeroInvestigacionPrevia = (
+    <div className="p-d-flex p-ai-center py-3 w-15">
+      <span>N° Investigación Previa</span>
+      <InputText
+        value={filters.numero_investigacion_previa.value || ''}
+        onChange={(e) => setFilters({ ...filters, numero_investigacion_previa: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS } })}
+        placeholder="Buscar"
+        className="px-5  text-gray-500 border border-gray-700 rounded-full h-8"
+      />
+    </div>
+  );
+
+  const headerNumeroInstruccionFiscal = (
+    <div className="p-d-flex p-ai-center py-3 w-15">
+      <span>N° Instrucción Fiscal</span>
+      <InputText
+        value={filters.numero_instruccion_fiscal.value || ''}
+        onChange={(e) => setFilters({ ...filters, numero_instruccion_fiscal: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS } })}
+        placeholder="Buscar"
+        className="px-5  text-gray-500 border border-gray-700 rounded-full h-8"
+      />
+    </div>
+  );
+
+  const actionBodyTemplate = (rowData) => {
+    return (
+        <React.Fragment>
+            <VscOpenPreview
+                title="Detalle"
+                className="h-7 w-7 text-blue-800 cursor-pointer inline-block mr-2"
+                onClick={() => navigate(`/delegaciones/detalleDelegacion/${rowData.id}`)}
+            />
+            <MdEditDocument
+                title="Actualizar"
+                className="h-7 w-7 text-green-600 cursor-pointer inline-block mr-2"
+                onClick={() => navigate(`/delegaciones/actualizarDelegacion/${rowData.id}`)}
+            />
+        </React.Fragment>
+    );
+};
+
+  const tableStyle = {
+    header: {
+      backgroundColor: '#203C6B',
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold', 
+        alignItems:'center'
+    }
+};
+  
   return (
-    <>
-      {delegaciones.length === 0 ? (
-        <Mensaje tipo={'active'}>{'No existen Delegaciones'}</Mensaje>
-      ) : (
-        <>
-          <Paginator
-            pageCount={pageCount}
-            handlePageClick={handlePageClick}
-            currentPage={currentPage}
-          />
-          <table className="w-full  table-auto shadow-lg bg-white">
-            <thead className="bg-sky-950 text-white">
-              <tr>
-                <th className="p-2 w-4">N° Orden</th>
-                <th className="p-3 w-7">Año Ingreso</th>
-                <th className="p-3 w-20">N° Instrucción Fiscal</th>
-                <th className="p-2">N° Investigación Previa</th>
-                <th className="p-2 w-3">Grado</th>
-                <th className="p-2">Agente</th>
-                <th className="p-2">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {delegaciones
-                .slice(currentPage * perPage, (currentPage + 1) * perPage)
-                .map((delegacion) => (
-                  <tr
-                    className="border-b hover:bg-gray-300 text-center"
-                    key={delegacion._id}
-                  >
-                    <td>{delegacion.orden}</td>
-                    <td>{delegacion.anio_ingreso}</td>
-                    <td>{delegacion.numero_instruccion_fiscal}</td>
-                    <td>{delegacion.numero_investigacion_previa}</td>
-                    <td>{delegacion.grado_agente}</td>
-                    <td>{delegacion.apellidos_nombres_agente}</td>
-                    <td className="py-2 text-center">
-                      <VscOpenPreview
-                        title="Detalle"
-                        className="h-7 w-7 text-blue-800 cursor-pointer inline-block mr-2"
-                        onClick={() =>
-                          navigate(`/delegaciones/detalleDelegacion/${delegacion.id}`)
-                        }
-                      />
-                      <MdEditDocument
-                        title="Actualizar"
-                        className="h-7 w-7 text-green-600 cursor-pointer inline-block mr-2"
-                        onClick={() =>
-                          navigate(`/delegaciones/actualizarDelegacion/${delegacion.id}`)
-                        }
-                      />
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </>
-      )}
-    </>
+    <div className=" flex w-full">
+      <DataTable
+        value={delegaciones}
+        sortField="orden"
+        sortOrder={1} // Ascendente
+        paginator
+        rows={10}
+        dataKey="id"
+        filters={filters}
+        filterDisplay="row"
+        loading={loading}
+        globalFilterFields={['numero_instruccion_fiscal', 'numero_investigacion_previa']}
+        emptyMessage="No Existe la Delegacion Buscada"
+          style={tableStyle.header}
+
+      >
+        <Column className="text-center w-5 text-black bg-white my-20 border-b border-r border-slate-200 " field="orden" header="N° Orden" />
+        <Column className="text-center w-15 text-black bg-white border-b  border-r border-slate-200" field="anio_ingreso" header="Año Ingreso" />
+        <Column className="text-center w-2 text-black bg-white border-b  border-r border-slate-200" header={headerNumeroInvestigacionPrevia} field="numero_investigacion_previa"  />
+        <Column className="text-center w-auto text-black bg-white border-b  border-r border-slate-200" header={headerNumeroInstruccionFiscal} field="numero_instruccion_fiscal" />
+        <Column className="text-center  text-black bg-white border-b  border-r border-slate-200" field="grado_agente" header="Grado" style={{ minWidth: '5rem' }}  />
+        <Column className="text-center w-auto my-15 text-black bg-white border-b  border-r border-gray-300 justify-center items-center" field="apellidos_nombres_agente" header="Agente" style={{ minWidth: '12rem'}} />
+        <Column className="text-center w-auto text-black bg-white  justify-center items-center border-b border-slate-200" body={actionBodyTemplate} header="Acciones" style={{ minWidth: '8rem' }} />
+
+      </DataTable>
+    </div>
   );
 };
 
--- app-ciberpol/src/paginas/Delegaciones.jsx
+++ app-ciberpol/src/paginas/Delegaciones.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import TablaDelegaciones from '../componets/TablaDelegaciones'
 import { useNavigate } from 'react-router-dom'
 import { IoSearch } from "react-icons/io5";
+import Asiganacion from '../componets/formularios/Asignacion';
+
 
 const Delegaciones = () => {
   const navigate = useNavigate();
@@ -10,27 +12,17 @@ const Delegaciones = () => {
       <h1 className='font-black text-4xl text-gray-500 '>DELEGACIONES</h1>
       <hr className='my-4 border-sky-950  '/>
 
-      <div className=' flex justify-center items-center mb-2'>
-        <div className='flex'>
-          <label className='mr-3 mt-1'>Buscar</label>
-          <input type="String" placeholder=" Instrución Fiscal/Investugación Previa" className=" rounded-md border border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-sky-900 py-1 px-2 text-gray-500" />
-          <IoSearch className='p-2 h-9 w-9 bg-blue-950 rounded-md border text-slate-200 mr-8' />
-        </div>
-
-
-
-      </div>
-
       <input
         type="submit"
-        className='bg-sky-950 w-auto p-3  text-slate-300 font-bold rounded-lg 
-                    hover:bg-blue-950 cursor-pointer transition-all ml-auto mb-3'
+        className=' bg-blue-900 w-auto p-3  text-white font-bold rounded-lg 
+                  hover:bg-gray-600 cursor-pointer transition-all ml-auto mb-5'
         onClick={() => navigate(`/delegaciones/nuevaDelegacion`)}
         value='Nueva Delegación'
       />
 
 
-      <TablaDelegaciones/>
+      <TablaDelegaciones/> 
+       
     
 
 
