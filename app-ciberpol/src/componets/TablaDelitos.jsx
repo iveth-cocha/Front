14a936f6dcc99d7a13f77f887e6a82f715877b27
ivet-cocha <maria.iveth16@outlook.es>
2024-05-23 00:24:35 -0500

    cambio tabla con filtro  Delitos

--- app-ciberpol/src/componets/TablaDelitos.jsx
+++ app-ciberpol/src/componets/TablaDelitos.jsx
@@ -1,127 +1,144 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { MdDeleteForever, MdEditDocument } from 'react-icons/md'
-import Mensaje from './Alertas/Mensaje'
-import ReactPaginate from 'react-paginate';
 
-const Paginator = ({ pageCount, handlePageClick, currentPage }) => {
-    return (
-      <ReactPaginate  className="flex flex-row flex-wrap justify-center  bg-sky-950 text-white italic py-2 rounded-t-lg border-b-2 border-gray-500"
-        previousLabel={'Anterior <'}
-        nextLabel={'> Siguiente'}
-        breakLabel={`... Página ${currentPage + 1} de ${pageCount} ...`}
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        containerClassName={'pagination'}
-        activeClassName={'active'}
-        previousClassName={'prev'}
-        nextClassName={'next'}
-      />
-    );
-  };
-  
+
+
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { FilterMatchMode, FilterService } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import axios from 'axios';
+import { MdDeleteForever } from 'react-icons/md';
+
   const TablaDelitos = () => {
-    const [tipDelitos, setTipDelitos] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [perPage] = useState(8); 
-    const [pageCount, setPageCount] = useState(0);
+    const navigate = useNavigate();
+
+    const [delitos, setDelitos] = useState(null);
+    const [filters, setFilters] = useState({
+      global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      delito: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      seccion: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
   
-    const handlePageClick = ({ selected }) => {
-      setCurrentPage(selected);
+    const [loading, setLoading] = useState(true);
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+  
+    const listarDelitos = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const url = `${import.meta.env.VITE_BACKEND_URL}/delitos`;
+        const options = {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const respuesta = await axios.get(url, options);
+        setDelitos(respuesta.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    useEffect(() => {
+      listarDelitos();
+    }, []);
+  
+    const onGlobalFilterChange = (e) => {
+      const value = e.target.value;
+      let _filters = { ...filters };
+  
+      _filters['global'].value = value;
+  
+      setFilters(_filters);
+      setGlobalFilterValue(value);
     };
   
-    const fetchData = async () => {
-        try {
-          const token = localStorage.getItem('token');
-          const url = `${import.meta.env.VITE_BACKEND_URL}/delitos`;
-          const options = {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`,
-            },
-          };
-          const respuesta = await axios.get(url, options);
-          setTipDelitos(respuesta.data);
-          setPageCount(Math.ceil(respuesta.data.length / perPage));
-        } catch (error) {
-          console.log(error);
-        }
-      };
+    const headerDelito = (
+      <div className="flex flex-col my-2  pl-36 justify-center items-center">
+        <span>Delito</span>
+        <InputText
+          value={filters.delito.value || ''}
+          onChange={(e) => setFilters({ ...filters, delito: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS } })}
+          placeholder="Buscar Delito"
+          className="px-5  text-gray-500 border border-gray-700 rounded-full h-8"
+        />
+      </div>
+    );
     
-      useEffect(() => {
-        fetchData();
-      }, [currentPage, perPage]);
-
+    const headerSeccion = (
+      <div className="flex flex-col my-2 pl-40 justify-center items-center">
+        <span className='' >Seccion</span>
+        <InputText
+          value={filters.seccion.value || ''}
+          onChange={(e) => setFilters({ ...filters, seccion: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS } })}
+          placeholder="Buscar"
+          className="px-5  text-gray-500 border border-gray-700 rounded-full h-8"
+        />
+      </div>
+    );
     const handleDelete = async (id) => {
-        try {
-            const confirmar = confirm("Eliminar el delito")
-            if (confirmar) {
-                const token = localStorage.getItem('token')
-                const url = `${import.meta.env.VITE_BACKEND_URL}/eliminar/delito/${id}`
-                const headers= {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${token}`
-                    }
-                await axios.delete(url, {headers});
-                fetchData();
-            }
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
+      try {
+          const confirmar = confirm("Eliminar el delito")
+          if (confirmar) {
+              const token = localStorage.getItem('token')
+              const url = `${import.meta.env.VITE_BACKEND_URL}/eliminar/delito/${id}`
+              const headers= {
+                      'Content-Type': 'application/json',
+                      Authorization: `Bearer ${token}`
+                  }
+              await axios.delete(url, {headers});
+              listarDelitos();
+          }
+      }
+      catch (error) {
+          console.log(error);
+      }
+  }
   
-    const displayDelitos = tipDelitos
-      .slice(currentPage * perPage, (currentPage + 1) * perPage)
-      .map((tipDelito, index) => (
-        <tr className="border-b hover:bg-gray-300 text-center px-8" key={tipDelito.id}>
-          <td>{index + 1}</td>
-          <td>{tipDelito.delito}</td>
-          <td>{tipDelito.seccion}</td>
-          <td className="py-2 text-center">
-            <MdEditDocument
-              title="Actualizar"
-              className="h-7 w-7 text-green-600 cursor-pointer inline-block mr-2"
-            />
-            <MdDeleteForever
-              onClick={() => handleDelete(tipDelito.id)}
+    const actionBodyTemplate = (rowData) => {
+      return (
+          <React.Fragment>
+              <MdDeleteForever
+              onClick={() => handleDelete(rowData.id)}
               className="h-7 w-7 text-red-900 cursor-pointer inline-block"
-            />
-          </td>
-        </tr>
-      ));
-
+                 
+              />
+              
+          </React.Fragment>
+      );
+  };
+  const tableStyle = {
+    header: {
+      backgroundColor: '#203C6B',
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold', 
+        alignItems:'center',
+  
+    }
+  };
     
-     
-
   
     return (
-      <>
-        {tipDelitos.length === 0 ? (
-          <Mensaje tipo={'active'}>{'No existen registros'}</Mensaje>
-        ) : (
-          <>
-          <Paginator 
-              pageCount={pageCount}
-              handlePageClick={handlePageClick}
-              currentPage={currentPage}
-            />
-            <table className="w-full table-auto shadow-lg bg-white">
-              <thead className="bg-sky-950 text-white">
-                <tr>
-                  <th className="p-2">N° </th>
-                  <th className="p-2">Delito </th>
-                  <th className="p-2">Sección</th>
-                  <th className="p-2">Acciones</th>
-                </tr>
-              </thead>
-              <tbody>{displayDelitos}</tbody>
-            </table>
-            
-          </>
-        )}
-      </>
+      <div className=" flex w-full">
+      <DataTable
+        value={delitos}
+        paginator rows={10}
+        dataKey="id" filters={filters} filterDisplay="row"
+        loading={loading} globalFilterFields={['delito', 'seccion']}
+        emptyMessage="No se encontró el delito ingresado"
+          style={tableStyle.header}
+
+      >
+        
+        <Column className="text-center px-3   text-black bg-white border-b  border-r border-slate-200" header={headerDelito} field="delito" style={{width:'35rem'}}  />
+        <Column className="text-center  text-black bg-white border-b  border-r border-slate-200" header={headerSeccion} field="seccion" style={{width:'40rem'}}/>
+        <Column className="text-center w-20 text-black bg-white  justify-center items-center border-b border-slate-200" body={actionBodyTemplate} header="Borrar"  />
+
+      </DataTable>
+    </div>
+     
     );
   };
   
